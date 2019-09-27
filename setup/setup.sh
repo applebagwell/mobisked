009263106a434ef0368027c25babd60d85bb6793
applebagwell <applebagwell@hotmail.com>
2019-09-27 17:08:30 -0400

    Added setup files.

---
+++ setup/setup.sh
@@ -0,0 +1,11 @@
+cd /media/skunky/apps
+wget -N https://www-eu.apache.org/dist/lucene/solr/8.2.0/solr-8.2.0.tgz
+wget -N https://www-us.apache.org/dist//jmeter/binaries/apache-jmeter-5.1.1.tgz
+wget -N https://www-us.apache.org/dist//ignite/2.7.6/apache-ignite-2.7.6-bin.zip
+wget -N https://www.archimatetool.com/downloads/4.5.1/Archi-Linux64-4.5.1.tgz
+wget -N https://www-eu.apache.org/dist/netbeans/netbeans/11.1/netbeans-11.1-bin.zip
+wget -N https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.3.2-linux-x86_64.tar.gz
+wget -N https://artifacts.elastic.co/downloads/kibana/kibana-7.3.2-linux-x86_64.tar.gz
+cd ~
+
+
