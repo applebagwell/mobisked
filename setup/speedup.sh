af1a9011d08f5d8ca9cc07305483e60399273f2c
applebagwell <applebagwell@hotmail.com>
2019-09-29 08:26:09 -0500

    Added speed up routines

---
+++ setup/speedup.sh
@@ -0,0 +1,10 @@
+sudo add-apt-repository ppa:apt-fast/stable
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get install apt-fast
+sudo apt-fast install preload
+sudo apt-fast install laptop-mode-tools
+
+sudo apt-get clean
+sudo apt-get autoremove
